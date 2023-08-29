93eda5a7d2c33b85c3dc88774eabc759e679215c
Noriuki <lanezupiroli@gmail.com>
2023-08-29 14:40:44 -0300

    asdasd

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiService = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.NEXT_API_URL || 'http://localhost:3030/api',
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
